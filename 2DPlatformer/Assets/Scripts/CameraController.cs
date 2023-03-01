f016fadf8bc58c9ab2484c6d7ca495ebe6acd0af
StarGrim33 <prosky33@gmail.com>
2023-03-01 14:26:11 +0300

    Выполнил задачу

--- 2DPlatformer/Assets/Scripts/CameraController.cs
+++ 2DPlatformer/Assets/Scripts/CameraController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(Character))]
+
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private float _speed;
